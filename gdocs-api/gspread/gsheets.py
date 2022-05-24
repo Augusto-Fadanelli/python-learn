fcba7287a895b3302bb89b6db0210190340c574b
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-05-24 11:40:04 -0300

    Update .gitignore, gsheets.py, and quickstart.py

--- gdocs-api/gspread/gsheets.py
+++ gdocs-api/gspread/gsheets.py
@@ -4,17 +4,24 @@
 # console.cloud.google.com/apis/
 
 # pip install gspread
+# Necessário token
 
 import gspread
 
 if __name__ == '__main__':
-    gc = gspread.service_account(filename='../.service_account.json')
+    gc = gspread.service_account(filename='.service_account.json')
     sh = gc.open_by_key('1Ise8zJkIX91XAnEBVrduFuha2kWdYqjuVdbOnvQGAh8')
 
     worksheet = sh.worksheet('Página1')
+
+    sheet = worksheet.get_all_records()
+    print(sheet)
+
+    '''
     while True:
-        # search = input('Insira a célula a ser pesquisada: ')
+        #search = input('Insira a célula a ser pesquisada: ')
         update = input('Insira a célula a ser substituída: ')
         print(update)
         worksheet.update(update, "teste")
         print(update)
+    '''
