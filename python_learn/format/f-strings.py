8c54e7ecd1b76777e8a10f44379c1381ab3e89ee
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-07-25 20:05:27 -0300

    add format

--- python_learn/format/f-strings.py
+++ python_learn/format/f-strings.py
@@ -1,3 +1,41 @@
+from datetime import datetime
+
 nome = 'Augusto'
 
 print(f'{nome.upper()}')
+
+print(f'{"Ração"!s}') # Chama string | É o mesmo que não colocar nada
+print(f'{"Ração"!r}') # Chama a representação
+print(f'{"Ração"!a}') # Converte o texto pra ASCII
+
+print(f'{nome=}') # Saída: nome='Augusto'
+
+produto = 'Ração'
+print(f'{produto.upper()=!a}') # Misturando as coisas
+
+# {:[preenchimento][alinhamento][tamanho_mínimo]}
+print(f'{"Ração":20} {"Augusto":10}')
+
+# Ex.: Tamanho
+nomes = ['Augusto', 'Eduardo', 'Nietzsche']
+idades = [22, 29, 100]
+
+for nome, idade in zip(nomes, idades):
+    print(f'{nome:12} {idade:5}')
+
+# Ex.: Alinhamento
+# < Esquerda | > Direita | ^ Centro
+for nome, idade in zip(nomes, idades):
+    print(f'{nome:>12} {idade:<5}')
+
+# Ex.: Preenchimento
+print(f'{" Menu ":-^30}') # preenchimento
+print(f'{"| 1 - Opção um":<29} {"|"}')
+print(f'{"| 2 - Opção dois":<29} {"|"}')
+print(f'{"| 3 - Opção três":<29} {"|"}')
+print('-'*30)
+
+# Datas
+agora = datetime.now()
+print(agora)
+print(f'{agora:%d/%m/%Y %Hhrs %Mmin %Ssec}')
--- python_learn/format/format.py
+++ python_learn/format/format.py
@@ -1,3 +1,5 @@
+from math import pi
+
 pessoa = {
         'nome': 'Augusto',
         'idade': 18,
@@ -7,3 +9,27 @@ pessoa = {
 s = 'Oi {nome}, vi que tem {idade} anos e seu apelido é {apelido}'
 
 print(s.format(**pessoa))
+
+print('{!s}'.format('Ração')) # Chama string | É o mesmo que não colocar nada
+print('{!r}'.format('Ração')) # Chama a representação
+print('{!a}'.format('Ração')) # Converte o texto pra ASCII
+
+# Conversão de bases
+print('{:o}'.format(258761276)) # Octal
+print('{:b}'.format(258761276)) # Binário
+print('{:x}'.format(258761276)) # Hexadecimal minúsculo
+print('{:X}'.format(258761276)) # Hexadecimal maiúsculo
+print('{:d}'.format(258761276)) # Decimal
+
+print('{:_^20x}'.format(258761276)) # Juntando tudo
+
+# Casas decimais
+print('{:.2f}'.format(pi))
+print('{:.^10.2f}'.format(pi))
+
+# Mostrar sinal
+print('{:+.2f}'.format(pi))
+print('{:+.2f}'.format(-pi))
+
+# Usando format
+print(format(pi, '_^20.2f'))
