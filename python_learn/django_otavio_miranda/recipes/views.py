575508a7fc669e50e8d52ff711042ae0557fe585
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-07-31 20:42:25 -0300

    Curso de Django - Otávio Miranda: Removido contato e sobre. Criado home.html

--- python_learn/django_otavio_miranda/recipes/views.py
+++ python_learn/django_otavio_miranda/recipes/views.py
@@ -1,13 +1,6 @@
-from django.http import HttpResponse # Retorna um response genérico
-#from django.shortcuts import render
+#from django.http import HttpResponse # Retorna um response genérico
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('HOME')
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
+    return render(request, 'recipes/home.html', context={'name': 'Augusto Fadanelli'})
 
