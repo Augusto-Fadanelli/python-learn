cd4e118fcdb0c63eae73546e0383e984760c5cb2
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-07-31 21:04:53 -0300

    Removido django_otavio_miranda. Add link do novo repositório onde se encontra o conteúdo.

--- python_learn/django_otavio_miranda/recipes/views.py
+++
@@ -1,6 +0,0 @@
-#from django.http import HttpResponse # Retorna um response genérico
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Augusto Fadanelli'})
-
