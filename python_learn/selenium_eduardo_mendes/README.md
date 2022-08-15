02d49fce7c66864a1874fcebfaf6641832794ea9
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-08-15 12:33:48 -0300

    Corrigido código da aula #03

--- python_learn/selenium_eduardo_mendes/README.md
+++ python_learn/selenium_eduardo_mendes/README.md
@@ -6,5 +6,6 @@
     ````
 
 ### Links úteis:
+  * [Documentação do Selenium](https://www.selenium.dev/documentation/)
   * [Curso de Selenium - Eduardo Mendes](https://dunossauro.github.io/curso-python-selenium/)
   * [Livro: Automate the boring stuff with python](https://automatetheboringstuff.com/)
--- python_learn/selenium_eduardo_mendes/aula.py
+++ python_learn/selenium_eduardo_mendes/aula.py
@@ -1,4 +1,8 @@
+from time import sleep
+
 from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 url = 'https://curso-python-selenium.netlify.app/aula_03.html#'
 
@@ -6,8 +10,13 @@ browser = Firefox()
 
 browser.get(url)
 
-#a = browser.find_element('name', 'a')
-#a.send_keys('geckodriver')
-#a.submit()
+sleep(1)
+
+a = browser.find_element(By.TAG_NAME, 'a')
+
+for click in range(10):
+    ps = browser.find_elements(By.TAG_NAME, 'p')
+    a.send_keys('geckodriver' + Keys.ENTER)
+    print(f'Valor do último p: {ps[-1].text} valor do click: {click}')
 
 #browser.quit()
